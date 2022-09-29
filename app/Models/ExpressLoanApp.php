cc228ea8e6585b0d0fb4321ad53c2496f7e9f500
venz0618 <100183442+venz0618@users.noreply.github.com>
2022-09-29 23:35:21 +0800

    asdasd

--- app/Models/ExpressLoanApp.php
+++ app/Models/ExpressLoanApp.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class ExpressLoanApp extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'acc_id',
+        'name',
+        'present_address',
+        'permanent_address',
+        'loan_type',
+        'emp',
+        'emp_address',
+        'email',
+        'amount',
+        'mode_payment',
+        'term_applied',
+        'phone_no',
+        'tin',
+        'fb_acc',
+
+    ];
 }
--- database/migrations/2022_09_24_171825_create_express_loan_apps_table.php
+++ database/migrations/2022_09_24_171825_create_express_loan_apps_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,21 @@ return new class extends Migration
     {
         Schema::create('express_loan_apps', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->string('acc_id');
+            $table->string('name');
+            $table->string('present_address');
+            $table->string('permanent_address');
+            $table->string('loan_type');
+            $table->string('emp');
+            $table->date('emp_address');
+            $table->string('email');
+            $table->string('amount');
+            $table->string('mode_payment');
+            $table->string('term_applied');
+            $table->string('phone_no');
+            $table->string('tin');
+            $table->string('fb_acc');
             $table->timestamps();
         });
     }
