252022f768820cb644f9f75bbc9aed89b4e4d011
Yousef <picturenow@hotmail.com>
2022-11-08 23:57:21 +0800

    made the about-us page

---
+++ resources/views/client/about-us/mission.blade.php
@@ -0,0 +1,5 @@
+<div class="col-md-7">
+    <h2 class="no-top-space">Mission</h2>
+    <h3>Provision of quality service to all CASALCO members and promotion of cooperativism 
+      with the highest ethical standard by being financially strong and responsive to the needs of its members constitute the primordial mission of CASALCO.</h3> 
+  </div>
---
+++ resources/views/client/about-us/vision.blade.php
@@ -0,0 +1,4 @@
+<div class="col-md-7">
+    <h2 class="no-top-space">Vision</h2>
+    <h3>CASALCO envision to become a competitive efficient and strong Cooperative serving the needs of its members.</h3> 
+  </div>
